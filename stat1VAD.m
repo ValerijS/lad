1d78e40c31f0e32b85ced098161e30220a359bae
ValerijS <bonvalsyn@gmail.com>
2015-04-10 00:06:30 +0300

    com-10-04-15

--- stat1VAD.m
+++ stat1VAD.m
@@ -1,10 +1,10 @@
-function mysimpleVAD(audifile)
+function stat1VAD(audifile)
 [ampl,frc] = audioread(audifile);
 numberFrames1=round(size(ampl)/(frc/20));
 numberFrames = numberFrames1(1);
 lengthFrame=round(frc/20);
 frame = ones(numberFrames,lengthFrame)';
-%preproframes = zeros(numberFrames,lengthFrame);
+
 for k = 1: numberFrames-1
     for n = 1 : lengthFrame
          frame(n,k) = ampl((k-1)* lengthFrame+n);
@@ -16,22 +16,22 @@ varNoice = ones(lengthFrame,1);
 for n = 1 : lengthFrame
     varNoice(n,1)= var(FRAME(n,1:min(20,round(numberFrames/5))));
 end 
+ind = 1:numberFrames;
 for k= 1:numberFrames
 ind(k) = sum(abs(FRAME(:,k)).^2./varNoice(:,1)-log(varNoice(:,1))-1);
 end
-
+vad1 = 1:numberFrames;
 for k = 1:numberFrames
   % max(ind(1:min(20,round(numberFrames/5))))
-   if ind(k) > 3.055*max(ind(1:min(20,round(numberFrames/5))))  %0.02;
+   if ind(k) > 60000 %9.55*max(ind(1:min(20,round(numberFrames/5))))  %0.02;
       vad1(k) =100;
    else   
       vad1(k) = 0;
    end
 end
-mean( varNoice(n,1))   
-mean(ind(1:min(20,round(numberFrames/5))))
-max((ind(1:min(20,round(numberFrames/5)))))
-%vad = zerosnumberFrames-1;
+%mean( varNoice(n,1))   
+%max(ind(1:min(20,round(numberFrames/5))))%vad = zerosnumberFrames-1;
+vad = ampl;
 for j = 1:numberFrames-1
   for k = 1:lengthFrame
     vad((j-1)*(lengthFrame-1) +k) = vad1(j);
